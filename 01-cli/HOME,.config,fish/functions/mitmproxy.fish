bb69021b48b02b4e12f42a5584f0a5d86014444e
Tomáš Jelínek <tomasjelinek@tutanota.com>
2021-05-24 23:15:40 +0200

    2021-05-24_23:15:40

--- 01-cli/HOME,.config,fish/functions/mitmproxy.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function mitmproxy --description 'alias mitmproxy mitmproxy --set confdir /home/jeli/.config/mitmproxy'
- command mitmproxy --set confdir /home/jeli/.config/mitmproxy $argv; 
-end
--- 01-cli/HOME,.config,fish/functions/mitmweb.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function mitmweb --description 'alias mitmweb mitmweb --set confdir /home/jeli/.config/mitmproxy'
- command mitmweb --set confdir /home/jeli/.config/mitmproxy $argv; 
-end
