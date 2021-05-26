f9ffd7cef1ee31ebd73036722256ab6761950959
Tomáš Jelínek <tomasjelinek@tutanota.com>
2021-05-26 17:40:20 +0200

    2021-05-26_17:40:20

--- 01-cli/HOME,.config,fish/functions/fetch_plugin.fish
+++ 01-cli/HOME,.config,fish/functions/fetch_plugin.fish
@@ -2,7 +2,6 @@
 function install_plugin
   rm -Rf "$XDG_CACHE_HOME/.fish_plugin" 2>/dev/null
   git clone --depth=1 "$argv[1]" "$XDG_CACHE_HOME/.fish_plugin"
-  mv "$XDG_CACHE_HOME/.fish_plugin/conf.d"/* "$XDG_CONFIG_HOME/fish/conf.d"
-  mv "$XDG_CACHE_HOME/.fish_plugin/functions"/* "$XDG_CONFIG_HOME/fish/functions"
+  mv -i "$XDG_CACHE_HOME/.fish_plugin/functions"/* "$XDG_CONFIG_HOME/fish/functions"
   rm -Rf "$XDG_CACHE_HOME/.fish_plugin" 2>/dev/null
 end
