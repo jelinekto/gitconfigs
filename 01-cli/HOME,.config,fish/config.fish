2bf20b7728713a31ddbce42cca012b9a6edf87b2
Tomáš Jelínek <tomasjelinek@tutanota.com>
2021-05-26 19:30:28 +0200

    2021-05-26_19:30:23

--- 01-cli/HOME,.config,fish/config.fish
+++ 01-cli/HOME,.config,fish/config.fish
@@ -1,7 +1,32 @@
-# do not process this file unless necessary
-status is-interactive; and not set -q __fish_config_set; or exit
+# only read this config when interactive
+status is-interactive; or exit
 
-# variables
+# Plugins
+## fzf
+bind \co __fzf_search_current_dir
+bind \cr __fzf_search_history
+bind --mode insert \co __fzf_search_current_dir
+bind --mode insert \cr __fzf_search_history
+## z
+function __z_on_variable_pwd --on-variable PWD
+    __z_add
+end
+if test ! -e "$Z_DATA"
+  mkdir -p "$Z_DATA"
+  and rmdir "$Z_DATA"
+  touch "$Z_DATA"
+end
+## pisces
+for pair in $pisces_pairs
+    _pisces_bind_pair insert (string split -- ',' $pair)
+end
+bind -M insert \b _pisces_backspace
+bind -M insert \177 _pisces_backspace
+bind -M insert \t _pisces_complete
+
+# do not read below this line unless necessary
+not set -q __fish_config_set; or exit
+
 set -U fish_greeting
 set -U fish_key_bindings fish_hybrid_key_bindings
 set -U fish_handle_reflow 0
