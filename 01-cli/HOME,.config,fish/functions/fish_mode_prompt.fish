aae01103bccc2be4a8974e00633a9973562615bb
Tomáš Jelínek <tomasjelinek@tutanota.com>
2021-05-27 11:25:17 +0200

    2021-05-27_11:25:17

--- 01-cli/HOME,.config,fish/functions/fish_mode_prompt.fish
+++ 01-cli/HOME,.config,fish/functions/fish_mode_prompt.fish
@@ -1,2 +1,14 @@
-function fish_mode_prompt # Disable default vi prompt
+function fish_mode_prompt
+  switch $fish_bind_mode
+    case default
+      echo -en "\e[1 q"
+    case insert
+      echo -en "\e[5 q"
+    case replace_one
+      echo -en "\e[3 q"
+    case visual
+      echo -en "\e[1 q"
+    case '*'
+      echo -en "\e[5 q"
+  end
 end
