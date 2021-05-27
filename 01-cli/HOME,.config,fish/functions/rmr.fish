fea4fb876401ad66325281e35b5e1661152f2442
Tomáš Jelínek <tomasjelinek@tutanota.com>
2021-05-27 15:55:48 +0200

    2021-05-27_15:55:48

--- 01-cli/HOME,.config,fish/functions/rmr.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function rmr --wraps='rm -R -I --one-file-system' --description 'alias rmr rm -R -I --one-file-system'
-  rm -R -I --one-file-system $argv; 
-end
--- 01-cli/HOME,.config,fish/functions/rmrf.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function rmrf --wraps='rm -Rf --one-file-system' --description 'alias rmrf rm -Rf --one-file-system'
-  rm -Rf --one-file-system $argv; 
-end
