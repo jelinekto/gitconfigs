c9853800430300ec7e41870f3da91d2ae3f129d3
Tomáš Jelínek <tomasjelinek@tutanota.com>
2021-05-30 10:57:11 +0200

    2021-05-30_10:57:11

--- 01-cli/HOME,.config,fish/functions/s.fish
+++ 01-cli/HOME,.config,fish/functions/s.fish
@@ -1,5 +1,5 @@
-# Defined via `source`
-function s --description 'alias sudo/doas'
+# Defined in /tmp/fish.cizAaK/s.fish @ line 2
+function s --wraps=doas --description 'alias doas'
   if test -n "$SUDO"
     $SUDO $argv
   else
