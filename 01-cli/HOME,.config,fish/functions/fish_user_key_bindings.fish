9df24151de49faa94f997a7f3b4f43bc352a30af
Tomáš Jelínek <tomasjelinek@tutanota.com>
2021-07-01 21:04:14 +0200

    2021-07-01_21:04:14

--- 01-cli/HOME,.config,fish/functions/fish_user_key_bindings.fish
+++ 01-cli/HOME,.config,fish/functions/fish_user_key_bindings.fish
@@ -9,8 +9,8 @@ bind           \co   __fzf_search_current_dir
 bind -M insert \co   __fzf_search_current_dir
 bind           \cr   __fzf_search_history
 bind -M insert \cr   __fzf_search_history
-bind           \e\cv $fzf_search_vars_cmd
-bind -M insert \e\cv $fzf_search_vars_cmd
+bind           \e\cv '__fzf_search_shell_variables (set --show | psub) (set --names | psub)'
+bind -M insert \e\cv '__fzf_search_shell_variables (set --show | psub) (set --names | psub)'
 bind           \e\cl __fzf_search_git_log
 bind -M insert \e\cl __fzf_search_git_log
 bind           \e\cs __fzf_search_git_status
