f63bb9f773f523a83353a94b3a71f04c787b0f67
Tomáš Jelínek <tomasjelinek@tutanota.com>
2021-07-07 20:00:51 +0200

    2021-07-07_20:00:51

--- 01-cli/HOME,.config,doom/config.el
+++ 01-cli/HOME,.config,doom/config.el
@@ -1,22 +1,13 @@
-; (setq user-full-name ""
-;       user-mail-address "")
-
+;; Fonts
 (setq doom-font (font-spec :family "Ubuntu Mono" :weight 'regular :size 22)
       doom-variable-pitch-font (font-spec :family "Ubuntu" :weight 'regular :size 22)
       doom-big-font (font-spec :family "Ubuntu" :weight 'regular :size 44))
 
-(setq doom-theme 'ewal-doom-one)
+;; pywal theme
+(setq doom-theme 'ewal-doom-vibrant)
 
+;; line numbering
 (setq display-line-numbers-type 'relative)
 
+;; org mode
 (setq org-directory "~/Documents")
-
-(use-package! ewal)
-
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
