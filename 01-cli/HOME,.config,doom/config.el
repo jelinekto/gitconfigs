6f165c0cd97b54d5553b7fd3faa4cdff5add2f7d
Tomáš Jelínek <tomasjelinek@tutanota.com>
2021-07-08 12:55:22 +0200

    2021-07-08_12:55:22

--- 01-cli/HOME,.config,doom/config.el
+++ 01-cli/HOME,.config,doom/config.el
@@ -1,10 +1,11 @@
 ;; Fonts
-(setq doom-font (font-spec :family "Ubuntu Mono" :weight 'regular :size 22)
-      doom-variable-pitch-font (font-spec :family "Ubuntu" :weight 'regular :size 22)
-      doom-big-font (font-spec :family "Ubuntu" :weight 'regular :size 44))
+(setq doom-font (font-spec :family "Ubuntu Mono" :weight 'light :size 21)
+      doom-variable-pitch-font (font-spec :family "Ubuntu" :weight 'light :size 21)
+      doom-big-font (font-spec :family "Ubuntu" :weight 'light :size 48))
 
 ;; pywal theme
-(setq doom-theme 'ewal-doom-vibrant)
+(add-to-list 'custom-theme-load-path "~/.config/doom")
+(setq doom-theme 'ewal-doom-custom)
 
 ;; line numbering
 (setq display-line-numbers-type 'relative)
